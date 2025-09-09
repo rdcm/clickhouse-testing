a59d71834a4378626650dd7872669a7cb1b05857
rdcm <rdcm@protonmail.ch>
2025-09-10 02:58:57 +0300

    handle different result types for tests

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -15,32 +15,42 @@ pub fn test(_args: TokenStream, input: TokenStream) -> TokenStream {
 
     let inner_name = syn::Ident::new(&format!("__{}_inner", func_name), func_name.span());
 
+    let handle_result = match func_output {
+        syn::ReturnType::Type(_, ty) if matches!(ty.as_ref(), syn::Type::Path(type_path) if type_path.path.segments.last().is_some_and(|seg| seg.ident == "Result")) =>
+        {
+            quote! {
+                match test_result {
+                    Ok(val) => {
+                        if let Err(e) = clickhouse_testing::cleanup_test(&client).await {
+                            panic!("Failed to cleanup test {:?} data: {:?}", #func_name_str, e);
+                        }
+                        val
+                    }
+                    Err(e) => panic!("Test {:?} failed: {:?}", #func_name_str, e),
+                }
+            }
+        }
+        _ => quote! {
+            let val = test_result;
+            if let Err(e) = clickhouse_testing::cleanup_test(&client).await {
+                panic!("Failed to cleanup test {:?} data: {:?}", #func_name_str, e);
+            }
+            val
+        },
+    };
+
     let expanded = quote! {
         #(#func_attrs)*
         #[tokio::test]
         #func_vis async fn #func_name() {
-            use futures::FutureExt;
-
             let module_path = module_path!();
             let client = match clickhouse_testing::init_test(module_path, #func_name_str).await {
                 Ok(c) => c,
                 Err(e) => panic!("Failed to setup test {:?} client: {:?}", #func_name_str, e),
             };
 
-            let test_result = std::panic::AssertUnwindSafe(async {
-                #inner_name(client.clone()).await
-            }).catch_unwind().await;
-
-            match test_result {
-                Ok(_) => {
-                    if let Err(e) = clickhouse_testing::cleanup_test(&client).await {
-                        panic!("Failed to cleanup test {:?} data: {:?}", #func_name_str, e);
-                    }
-                }
-                Err(panic_info) => {
-                    std::panic::resume_unwind(panic_info);
-                }
-            }
+            let test_result = #inner_name(client.clone()).await;
+            #handle_result
         }
 
         async fn #inner_name(#func_inputs) #func_output #func_block
