123181f8b0f53ca4358cc56e8efcef5d3e4e705a
a.smirnov <a.smirnov@sbs-tech.ru>
2025-10-16 10:41:05 +0300

    Sanitize Database Name

--- core/tests/example.rs
+++ core/tests/example.rs
@@ -1,3 +1,5 @@
+use rstest::rstest;
+
 #[clickhouse_testing::test]
 async fn test1(client: clickhouse_testing::Client) {
     let version = client
@@ -18,4 +20,19 @@ async fn test2(client: clickhouse_testing::Client) {
         .unwrap();
 
     assert_eq!(version, "25.5.2.47");
+}
+
+#[rstest]
+#[case("SELECT toString(2 + 2)", "4")]
+#[case("SELECT toString(10 * 5)", "50")]
+#[case("SELECT 'hello'", "hello")]
+#[clickhouse_testing::test]
+async fn rstest_successfully_cases(
+    #[ignore] client: clickhouse_testing::Client,
+    #[case] query: &str,
+    #[case] expected: &str,
+) {
+    let result: String = client.query(query).fetch_one().await.unwrap();
+
+    assert_eq!(result, expected);
 }
